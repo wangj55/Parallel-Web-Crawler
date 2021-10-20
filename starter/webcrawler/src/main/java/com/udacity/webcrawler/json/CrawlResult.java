1bb1900872582ada53c79039a2b70cfe1b7653a9
Ji Wang <wj42421999@gmail.com>
2021-10-20 16:51:53 -0400

    finish crawler output

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResult.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResult.java
@@ -10,76 +10,76 @@ import java.util.Objects;
  */
 public final class CrawlResult {
 
-  private final Map<String, Integer> wordCounts;
-  private final int urlsVisited;
-
-  /**
-   * Constructs a {@link CrawlResult} with the given word counts and visited URL count.
-   */
-  private CrawlResult(Map<String, Integer> wordCounts, int urlsVisited) {
-    this.wordCounts = wordCounts;
-    this.urlsVisited = urlsVisited;
-  }
-
-  /**
-   * Returns an unmodifiable {@link Map}. Each key in the map is a word that was encountered
-   * during the web crawl. Each value is the total number of times a word was seen.
-   *
-   * <p>When computing these counts for a given crawl, results from the same page are never
-   * counted twice.
-   *
-   * <p>The size of the returned map is the same as the {@code "popularWordCount"} option in the
-   * crawler configuration. For example,  if {@code "popularWordCount"} is 3, only the top 3 most
-   * frequent words are returned.
-   *
-   * <p>If multiple words have the same frequency, prefer longer words rank higher. If multiple
-   * words have the same frequency and length, use alphabetical order to break ties (the word that
-   * comes first in the alphabet ranks higher).
-   */
-  public Map<String, Integer> getWordCounts() {
-    return wordCounts;
-  }
-
-  /**
-   * Returns the number of distinct URLs the web crawler visited.
-   *
-   * <p>A URL is considered "visited" if the web crawler attempted to crawl that URL, even if the
-   * HTTP request to download the page returned an error.
-   *
-   * <p>When computing this value for a given crawl, the same URL is never counted twice.
-   */
-  public int getUrlsVisited() {
-    return urlsVisited;
-  }
-
-  /**
-   * A package-private builder class for constructing web crawl {@link CrawlResult}s.
-   */
-  public static final class Builder {
-    private Map<String, Integer> wordFrequencies = new HashMap<>();
-    private int pageCount;
+    private final Map<String, Integer> wordCounts;
+    private final int urlsVisited;
 
     /**
-     * Sets the word counts. See {@link #getWordCounts()}
+     * Constructs a {@link CrawlResult} with the given word counts and visited URL count.
      */
-    public Builder setWordCounts(Map<String, Integer> wordCounts) {
-      this.wordFrequencies = Objects.requireNonNull(wordCounts);
-      return this;
+    private CrawlResult(Map<String, Integer> wordCounts, int urlsVisited) {
+        this.wordCounts = wordCounts;
+        this.urlsVisited = urlsVisited;
     }
 
     /**
-     * Sets the total number of URLs visited. See {@link #getUrlsVisited()}.
+     * Returns an unmodifiable {@link Map}. Each key in the map is a word that was encountered
+     * during the web crawl. Each value is the total number of times a word was seen.
+     *
+     * <p>When computing these counts for a given crawl, results from the same page are never
+     * counted twice.
+     *
+     * <p>The size of the returned map is the same as the {@code "popularWordCount"} option in the
+     * crawler configuration. For example,  if {@code "popularWordCount"} is 3, only the top 3 most
+     * frequent words are returned.
+     *
+     * <p>If multiple words have the same frequency, prefer longer words rank higher. If multiple
+     * words have the same frequency and length, use alphabetical order to break ties (the word that
+     * comes first in the alphabet ranks higher).
      */
-    public Builder setUrlsVisited(int pageCount) {
-      this.pageCount = pageCount;
-      return this;
+    public Map<String, Integer> getWordCounts() {
+        return wordCounts;
     }
 
     /**
-     * Constructs a {@link CrawlResult} from this builder.
+     * Returns the number of distinct URLs the web crawler visited.
+     *
+     * <p>A URL is considered "visited" if the web crawler attempted to crawl that URL, even if the
+     * HTTP request to download the page returned an error.
+     *
+     * <p>When computing this value for a given crawl, the same URL is never counted twice.
      */
-    public CrawlResult build() {
-      return new CrawlResult(Collections.unmodifiableMap(wordFrequencies), pageCount);
+    public int getUrlsVisited() {
+        return urlsVisited;
     }
-  }
+
+    /**
+     * A package-private builder class for constructing web crawl {@link CrawlResult}s.
+     */
+    public static final class Builder {
+        private Map<String, Integer> wordFrequencies = new HashMap<>();
+        private int pageCount;
+
+        /**
+         * Sets the word counts. See {@link #getWordCounts()}
+         */
+        public Builder setWordCounts(Map<String, Integer> wordCounts) {
+            this.wordFrequencies = Objects.requireNonNull(wordCounts);
+            return this;
+        }
+
+        /**
+         * Sets the total number of URLs visited. See {@link #getUrlsVisited()}.
+         */
+        public Builder setUrlsVisited(int pageCount) {
+            this.pageCount = pageCount;
+            return this;
+        }
+
+        /**
+         * Constructs a {@link CrawlResult} from this builder.
+         */
+        public CrawlResult build() {
+            return new CrawlResult(Collections.unmodifiableMap(wordFrequencies), pageCount);
+        }
+    }
 }
